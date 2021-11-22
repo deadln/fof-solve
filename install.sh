f06ed3203f2447b92ca313ffc04c9b391d238c7b
korotkoves <korotkov.es@mipt.ru>
2021-11-22 19:47:44 +0300

    fix

--- install.sh
+++ install.sh
@@ -9,24 +9,31 @@ if [ "$1" == "update" ]; then
  exit
 fi
 
-sudo ./multiple-sitl/install/ros/sudo_deps.sh
-cat ./deps/apt.txt | sudo xargs apt install -y
+if [ "$1" != "prepare" ]; then
+  sudo ./multiple-sitl/install/ros/sudo_deps.sh
+  cat ./deps/apt.txt | sudo xargs apt install -y
 
-while read -a p; do
- pip install $p
-done < ./deps/pip.txt
+  while read -a p; do
+   pip install $p
+  done < ./deps/pip.txt
+fi
 
 cws="$HOME/catkin_ws"
 rosinstall_file=""
 
 if [ ! -d $cws ];then
   ./multiple-sitl/install/catkin_prepare.sh $cws $rosinstall_file
-  str="source $cws/devel/setup.bash"
-  rcfile=~/.bashrc
+fi
 
+srcs="/opt/ros/noetic/setup.bash $cws/devel/setup.bash"
+rcfile=~/.bashrc
+
+for s in $srcs
+do
+  str="source $s"
   grep "$str" $rcfile >/dev/null
   if [ $? -eq 1 ];then
     echo $str >> $rcfile
     source ~/.bashrc
   fi
-fi
+done
